ec32fd4c0998c0027853b5c05dff04671ab3f415
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-15 15:09:47 +0530

    [Raja]Add Retrieving  the data from a Table  for the Employee payroll

--- EmployeepayrollServices.sql
+++ EmployeepayrollServices.sql
@@ -8,9 +8,11 @@ Salary float,
 StartDate Date
 );
 /*UC3-Insert a data into the table */
-Insert into Employee_payroll(Name,Salary,StartDate)
-Values('Raja',339472,'5-6-2016'),
+Insert into Employeepayroll(Name,Salary,StartDate)
+values('Raja',339472,'5-6-2016'),
 ('Nari',567823,'3-7-2016'),
 ('Prabha',456735,'8-9-2017'),
 ('Nikkitha',458732,'10-4-2018'),
 ('Rasi',568239,'12-5-2019');
+/* UC4-Retrieve the data from the table*/
+SELECT * FROM Employeepayroll
