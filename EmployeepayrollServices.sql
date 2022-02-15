db423d5f2b367d0a5617bbfaba0fda5ea441c0f8
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-15 14:50:09 +0530

    [Raja]Add Creating a Table  for the Employee payroll

--- EmployeepayrollServices.sql
+++ EmployeepayrollServices.sql
@@ -1,2 +1,9 @@
 /* UC1 creating database */
 create database Employeepayroll_services
+/* UC2 creating Table for Employee  */
+create table Employeepayroll(
+Id int IDENTITY(1,1) PRIMARY KEY,
+Name varchar(200),
+Salary float,
+StartDate Date
+);
