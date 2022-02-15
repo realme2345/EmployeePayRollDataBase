909efbec39783da90771ed264821fb96a446614c
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-15 14:58:26 +0530

    [Raja]Add inserting the data into a Table  for the Employee payroll

--- EmployeepayrollServices.sql
+++ EmployeepayrollServices.sql
@@ -7,3 +7,10 @@ Name varchar(200),
 Salary float,
 StartDate Date
 );
+/*UC3-Insert a data into the table */
+Insert into Employee_payroll(Name,Salary,StartDate)
+Values('Raja',339472,'5-6-2016'),
+('Nari',567823,'3-7-2016'),
+('Prabha',456735,'8-9-2017'),
+('Nikkitha',458732,'10-4-2018'),
+('Rasi',568239,'12-5-2019');
