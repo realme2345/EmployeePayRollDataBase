64642ebda01d9769f16bf658eefb2e94109f63d2
realme2345 <95670571+realme2345@users.noreply.github.com>
2022-02-15 15:41:49 +0530

    [Raja]Add calculate sum avg and max and min value from a Table  for the Employee payroll

--- EmployeepayrollServices.sql
+++ EmployeepayrollServices.sql
@@ -16,9 +16,28 @@ values('Raja',339472,'5-6-2016'),
 ('Rasi',568239,'12-5-2019');
 /* UC4-Retrieve the data from the table*/
 SELECT * FROM Employeepayroll
+/* UC-5 get the data of particular person from the table */
 SELECT * FROM Employeepayroll where Name='Raja';
 SELECT * FROM Employeepayroll where Id=2 or Id=5;
 SELECT * FROM Employeepayroll where STARTDATE BETWEEN CAST('2016-05-06' as date) AND GETDATE();
+/* UC6 Alter the new coloumn to the table */
 Alter table Employeepayroll add Gender char;
 UPDATE Employeepayroll SET Gender='F' where id=4 or Id=5;
 SELECT * FROM Employeepayroll
+/* Uc-7 performing sum avg and max and min salaries from the table */
+SELECT SUM(Salary) as Totalsalary FROM Employeepayroll;
+SELECT SUM(Salary) as Totalsalary, Gender FROM Employeepayroll group by Gender;
+SELECT SUM(Salary) as Femalesalary FROM Employeepayroll where Gender='F';
+SELECT SUM(Salary) as malesalary FROM Employeepayroll where Gender='M';
+SELECT AVG(Salary) as Avgsalry FROM Employeepayroll;
+SELECT MAX(Salary) as Maximumsalray FROM Employeepayroll;
+SELECT MAX(Salary) as Maximumsalary, Gender FROM Employeepayroll group by Gender;
+SELECT MIN(Salary) as Minimumsalray FROM Employeepayroll;
+SELECT MIN(Salary) as Minimumsalary, Gender FROM Employeepayroll group by Gender;
+
+
+
+
+
+
+
